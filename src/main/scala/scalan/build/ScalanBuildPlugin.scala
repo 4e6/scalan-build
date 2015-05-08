496016d9bd760cb09b14f9cbdc5d8d5a83fba943
Dmitry Bushev <bushevdv@gmail.com>
2015-05-08 12:50:19 +0300

    add scripted tests

--- src/main/scala/scalan/build/ScalanBuildPlugin.scala
+++ src/main/scala/scalan/build/ScalanBuildPlugin.scala
@@ -12,7 +12,7 @@ object ScalanBuildPlugin extends AutoPlugin {
 
   private def scalanSettings = Seq(
     gitRevision := ScalanBuild.gitRev.get,
-    commands ++= Seq(snapshotRevision)
+    commands ++= Seq(versionFixed)
   )
 
   private def scalaSettings = Seq(
@@ -30,28 +30,23 @@ object ScalanBuildPlugin extends AutoPlugin {
 
 object ScalanBuild {
 
+  type Revision = String
+
   val RevLength = 7
 
-  def snapshotRevision = Command.command("snapshotRevision") { state =>
+  def versionFixed = Command.command("versionFixed") { state =>
     val extracted = Project extract state
     val aggregates = aggregate(state, withCurrent = true)
-    val snapshotVersion = mkSnapshotVersion(state, envRev orElse gitRev)
+    val revision = envRev orElse gitRev map { format(_) }
+    val fixedVersion = fixVersion(state, revision)
 
-    state.log.info(s"Set version to $snapshotVersion")
+    state.log.info(s"Set version to $fixedVersion")
 
     val settings = (Seq.empty[Def.Setting[String]] /: aggregates) { (s, ref) =>
-      s :+ (version in ref := snapshotVersion)
+      s :+ (version in ref := fixedVersion)
     }
 
     extracted.append(settings, state)
-  }
-
-  def gitRev: Option[String] = {
-    util.Try(s"git rev-parse --short=$RevLength HEAD".lines_!.head).toOption
-  }
-
-  def envRev: Option[String] = {
-    sys.env.get("VCS_REVISION").map(_ take RevLength)
   }
 
   def aggregate(state: State, withCurrent: Boolean): Seq[ProjectRef] = {
@@ -60,9 +55,20 @@ object ScalanBuild {
     if (withCurrent) currentRef +: aggs else aggs
   }
 
-  private def mkSnapshotVersion(state: State, revision: Option[String]): String = {
+  def gitRev: Option[Revision] =
+    util.Try(s"git rev-parse HEAD".lines_!.head).toOption
+
+  def envRev: Option[Revision] =
+    sys.env.get("VCS_REVISION")
+
+  def format(rev: Revision, length: Int = RevLength): Revision =
+    rev take length
+
+  def fixVersion(state: State, revision: Option[Revision]): String = {
     val x = Project.extract(state); import x._
     val version = get(Keys.version)
-    revision.fold(version)(rev => if (get(isSnapshot)) s"$version-$rev" else version)
+    revision.fold(version) { rev =>
+      if (get(isSnapshot)) s"$version-$rev" else version
+    }
   }
 }
